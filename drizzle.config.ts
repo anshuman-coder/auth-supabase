b94f0dd00005a6aea1394ed37ab3e92f455243e2
anshuman-coder <anshurockingboy23@gmail.com>
2024-11-03 01:37:26 +0530

    schema migration and working

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,5 +8,5 @@ export default {
   dbCredentials: {
     url: env.DATABASE_URL,
   },
-  tablesFilter: ["auth-supabase_*"],
+  tablesFilter: ["*"],
 } satisfies Config;
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -3,10 +3,13 @@
 
 import { sql } from "drizzle-orm";
 import {
+  foreignKey,
   index,
   integer,
   pgTableCreator,
   timestamp,
+  uniqueIndex,
+  uuid,
   varchar,
 } from "drizzle-orm/pg-core";
 
@@ -16,7 +19,7 @@ import {
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator((name) => `auth-supabase_${name}`);
+export const createTable = pgTableCreator((name) => `${name}`);
 
 export const posts = createTable(
   "post",
@@ -34,3 +37,56 @@ export const posts = createTable(
     nameIndex: index("name_idx").on(example.name),
   })
 );
+
+export const user = createTable(
+  "user",
+  {
+    id: uuid("id").primaryKey().defaultRandom().notNull(),
+    name: varchar("name", { length: 255 }).notNull(),
+    email: varchar("email", { length: 255 }).notNull(),
+    emailVerified: timestamp("emailVerified", { withTimezone: true, mode: 'date' }),
+  }
+)
+
+export const account = createTable(
+  "account",
+  {
+    id: uuid("id").defaultRandom().primaryKey().notNull(),
+    userId: uuid("user_id").notNull(),
+    provider: varchar("provider", { length: 255 }).notNull(),
+    providerAccountId: varchar("provider_account_id", { length: 255 }).notNull(),
+    refresh_token: varchar("refresh_token", { length: 255 }),
+    access_token: varchar("access_token", { length: 255 }),
+    expires_at: integer("expires_at"),
+    token_type: varchar("token_type", { length: 255 }),
+    scope: varchar("scope", { length: 255 }),
+    id_token: varchar("id_token"),
+    session_state: varchar("session_state", { length: 255 }),
+  }, (t) => ({
+    fk: foreignKey({
+      name: "account_user_fk",
+      columns: [t.userId],
+      foreignColumns: [user.id],
+    })
+    .onDelete("cascade"),
+    uniqueProviderAccount: uniqueIndex("account_provider_providerAccountId_unique").on(t.provider, t.providerAccountId),
+  })
+)
+
+export const session = createTable(
+  "session",
+  {
+    id: uuid("id").defaultRandom().primaryKey().notNull(),
+    userId: uuid("user_id").notNull(),
+    sessionToken: varchar("session_token", { length: 255 }).notNull().unique(),
+    expires: timestamp("expires", { withTimezone: true }).notNull(),
+  }, (t) => ({
+    fk: foreignKey({
+      name: "session_user_fk",
+      columns: [t.userId],
+      foreignColumns: [user.id],
+    })
+    .onDelete("cascade"),
+    uniqueSessionToken: uniqueIndex("session_sessionToken_unique").on(t.sessionToken),
+  })
+)
